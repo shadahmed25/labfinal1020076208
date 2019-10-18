ab90d3263ab9812e3186b79766b3004ebc23a64f
shadahmed25 <shad.ahmed25@gmail.com>
2019-10-18 22:21:56 +0600

    Adds handling for case when user enters invalid arguments

--- Constants.java
+++ Constants.java
@@ -4,5 +4,12 @@ package ase;
      public  static  final  String DataFile = "students.txt"; 
      public  static  final  String EndDialog = "Data Loaded."; 
   public  static  final  String Success = "We found it!";
+ 		public  static  final  String ShowAll = "a"; 
+     public  static final  String ShowRandow = "r"; 
+    public  static  final  String count = "c"; 
+    public  static final  String addWord = "+"; 
+     public  static  final  String query = "?"; 
+     public  static final String DateFormat = "dd/mm/yyyy-hh:mm:ss a"; 
+
 }
 
